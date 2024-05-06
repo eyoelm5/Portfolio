1a6cb9bb28bdef9c74706f6c8b676c520fc858d0
Eyoel Moges <eyoelmoges012@gmail.com>
2024-05-07 00:04:03 +0300

    Final Version

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,21 @@
 const initApp = () => {
     const hamburgerBtn = document.getElementById('hamburger-btn')
     const mobileMenu = document.getElementById('mobile-menu')
+    const workwithme = document.getElementById('btn--landing-page')
+    
+    function contact(){
+        window.location.href="./contact.html";
+    }
 
     const toggleMenu = () => {
         mobileMenu.classList.toggle('hidden')
         mobileMenu.classList.toggle('block')
         hamburgerBtn.classList.toggle('toggle-btn')
     }
-
     hamburgerBtn.addEventListener('click', toggleMenu)
     mobileMenu.addEventListener('click', toggleMenu)
+    workwithme.addEventListener('click', contact)
 }
+
 
 document.addEventListener('DOMContentLoaded', initApp)
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./build/*.html","./build/js/*.js"],
   theme: {
-    extend: {},
-  plugins: [],
+    extend: {
+      keyframes:{
+        'spin':{
+          from: {transform: 'rotate(0deg)'},
+          to: { transform: 'rotate(360deg)'}
+        }
+      },
+      animation: {
+        'spin': '10s linear spin infinite'
+      }
+    },
+    plugins: [],
+  }
 }
-}
-
